bc58c124121eb3e3f0c45d627cb2d00c41226521
jsanto <jsanto@fq-wireless-pittnet-150-212-2-73.wireless.pitt.edu>
2015-04-07 14:31:18 -0400

    Initial Commit

---
+++ JJSAssignment 6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JJSAssignment 6
+//
+//  Created by jsanto on 4/7/15.
+//  Copyright (c) 2015 Jason Santo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
